76522a2ba8843da6e2cd5a49991825578a28ced6
liuzonghai8 <liuzonghai8@163.com>
2020-06-13 23:15:16 +0800

    register - end

--- app/src/main/java/com/example/maintain/login/LoginViewModel.java
+++ app/src/main/java/com/example/maintain/login/LoginViewModel.java
@@ -1,15 +1,13 @@
 package com.example.maintain.login;
 
 import android.app.Application;
-import android.content.ContentProvider;
 import android.content.Context;
-import android.content.SharedPreferences;
+import android.text.TextUtils;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.MutableLiveData;
-import androidx.lifecycle.ViewModel;
 
 import com.example.maintain.R;
 import com.example.maintain.utils.EncryptUtil;
@@ -21,20 +19,20 @@ public class LoginViewModel extends AndroidViewModel {
     private static final String TAG_LOG = "Tag_log";
     private MutableLiveData<String> username;
     private MutableLiveData<Boolean> valid;
+    //edit_key
     private MutableLiveData<String> userKey;
-    //    private MutableLiveData
-     //key
+    public MutableLiveData<Boolean> flag;
+    //key
     private final String keyName = getApplication().getString(R.string.shared_key);
     //file
     private final String fileName = getApplication().getString(R.string.shared_file_name);
     //device NO.
     public final String device = MobileInfoUtil.getIMEI(getApplication());
+    private Context content = getApplication();
 
     public LoginViewModel(@NonNull Application application) {
         super(application);
     }
-
-
     //检查账号是否合法
     public boolean checkPhone() {
         String user = username.getValue();
@@ -48,27 +46,33 @@ public class LoginViewModel extends AndroidViewModel {
 
     // 永久保存key
     public void saveKey() {
-        Context content = getApplication();
-         SharedData sd = new SharedData(content);
-         sd.save(fileName,keyName,userKey.getValue());
-        Log.d("TAG_LOG", "---saveKey--ok--");
+//        Context content = getApplication();
+        SharedData sd = new SharedData(content);
+        if (flag.getValue()) {
+            sd.save(fileName, keyName, userKey.getValue());
+            flag.setValue(false);
+        } else {
+            sd.save(fileName, keyName, "");
+            flag.setValue(true);
+        }
+    }
+
+    public String getKey() {
+        //从文件中读取key
+        SharedData sd = new SharedData(content);
+        return sd.load(fileName, keyName);
     }
 
     //比较Key
     public Boolean compareKey() {
-        //从文件中读取key
-        Context content = getApplication();
-        SharedData sd = new SharedData(content);
-        String key = sd.load(fileName,keyName);
-        //Log.d("TAG_LOG","-----load key-----"+key);
         //计算key
         String result = EncryptUtil.calculateKey(username.getValue(), device);
-       // Log.d("TAG_LOG","-----calculate key-----"+result);
-
-      //  String s = EncryptUtil.calculateKey("18812345678", "f3a2d2abfa17862f");
-     //   Log.d("TAG_LOG","-----测试s.-----"+s);
+        // Log.d("TAG_LOG","-----calculate key-----"+result);
+        String key = getKey();
+        String s = EncryptUtil.calculateKey("18812345678", "f3a2d2abfa17862f");
+        Log.d("TAG_LOG", "-----测试s.-----" + s);
         //比较两个可以
-        return result.equals(key);
+        return EncryptUtil.calculateKey(username.getValue(), device).equals(getKey());
     }
 
     public MutableLiveData<Boolean> getValid() {
@@ -101,4 +105,17 @@ public class LoginViewModel extends AndroidViewModel {
         this.userKey = userKey;
     }
 
+    public MutableLiveData<Boolean> getFlag() {
+        if (flag == null) {
+            flag = new MutableLiveData<Boolean>();
+            flag.setValue(false);
+            Log.d("TAG_LOG", "---getFlag --初始化 ---false-");
+        }
+        if (getKey() == "" || getKey() == null) {
+            Log.d("TAG_LOG", "---getFlag--- getkey---:" + getKey().toString());
+            flag.setValue(true);
+            Log.d("TAG_LOG", "---getFlag--- getkey---false-");
+        }
+        return flag;
+    }
 }
--- app/src/main/java/com/example/maintain/login/RegisterActivity.java
+++ app/src/main/java/com/example/maintain/login/RegisterActivity.java
@@ -1,13 +1,18 @@
 package com.example.maintain.login;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.maintain.R;
-import com.example.maintain.databinding.ActivityLoginBinding;
 import com.example.maintain.databinding.ActivityRegisterBinding;
 
 public class RegisterActivity extends AppCompatActivity {
@@ -22,9 +27,37 @@ public class RegisterActivity extends AppCompatActivity {
         binding= DataBindingUtil.setContentView(this,R.layout.activity_register);
         // Get the ViewModel.
         model = new ViewModelProvider(this).get(LoginViewModel.class);
-
+        Log.d("TAG_LOG","---model.getFlag().getValue()--"+model.getFlag().getValue());
         binding.setData(model);
         binding.setLifecycleOwner(this);
+        //butListener();
 
+        model.getFlag().observe(this, new Observer<Boolean>() {
+            @Override
+            public void onChanged(Boolean flag) {
+                Log.d("TAG_LOG","---flag-observe--"+flag);
+                if(flag){
+                    binding.butSave.setText("保存Key");
+                    //binding.eidtKey.setVisibility();
+                }else {
+                    binding.butSave.setText("清除Key");
+
+                }
+            }
+        });
     }
+
+
+
+//    //buttom按键
+//    private void butListener(){
+//        binding.butSave.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                binding.butSave.setText("save");
+//            }
+//        });
+//    }
+
+
 }
